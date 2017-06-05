77c5419b9a176fa44aa093a6846cc2406e368757
李霽烝 <GJen@lijizhengde-MacBook-Pro.local>
2017-06-05 15:53:00 +0800

    plate regonition / route plan / comment system

--- app/src/main/java/com/example/gjen/taxibar/MainActivity.java
+++ app/src/main/java/com/example/gjen/taxibar/MainActivity.java
@@ -1,136 +1,46 @@
 package com.example.gjen.taxibar;
 
-import android.content.ContentResolver;
-import android.content.ContentValues;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Matrix;
-import android.net.Uri;
-import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    //宣告
-    private ImageView mImg;
-    private DisplayMetrics mPhone;
-    private final static int CAMERA = 66 ;
-    private final static int PHOTO = 99 ;
-
+    String userName = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //讀取手機解析度
-        mPhone = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(mPhone);
-
-        mImg = (ImageView) findViewById(R.id.img);
-        Button mCamera = (Button) findViewById(R.id.camera);
-        Button mPhoto = (Button) findViewById(R.id.photo);
-
-        mCamera.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                // 開啟相機功能，並將拍照後的圖片存入SD卡相片集內，須由startActivityForResult且帶入
-                // requestCode進行呼叫，原因為拍照完畢後返回程式後則呼叫onActivityResult
-                ContentValues value = new ContentValues();
-                value.put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg");
-                Uri uri= getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                        value);
-                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
-                intent.putExtra(MediaStore.EXTRA_OUTPUT, uri.getPath());
-                startActivityForResult(intent, CAMERA);
-            }
-        });
-
-        mPhoto.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                // 開啟相簿相片集，須由startActivityForResult且帶入requestCode進行呼叫，原因
-                // 為點選相片後返回程式呼叫onActivityResult
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(intent, PHOTO);
-            }
-        });
+        getBundle();
     }
 
-    //拍照完畢或選取圖片後呼叫此函式
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode,Intent data)
-    {
-        //藉由requestCode判斷是否為開啟相機或開啟相簿而呼叫的，且data不為null
-        if ((requestCode == CAMERA || requestCode == PHOTO ) && data != null)
-        {
-            //取得照片路徑uri
-            Uri uri = data.getData();
-            ContentResolver cr = this.getContentResolver();
-
-            try
-            {
-                //讀取照片，型態為Bitmap
-                Bitmap bitmap;
-
-                if(data.getData()==null){
-                    bitmap = (Bitmap)data.getExtras().get("data");
-                }else{
-                    bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), data.getData());
-                }
-
-                //判斷照片為橫向或者為直向，並進入ScalePic判斷圖片是否要進行縮放
-                if(bitmap.getWidth()>bitmap.getHeight())ScalePic(bitmap,
-                        mPhone.heightPixels);
-                else ScalePic(bitmap,mPhone.widthPixels);
-            }
-            catch (FileNotFoundException e)
-            {
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        super.onActivityResult(requestCode, resultCode, data);
+    private void getBundle(){
+        Bundle bundle = this.getIntent().getExtras();
+        userName = bundle.getString("userName");
     }
 
-    private void ScalePic(Bitmap bitmap,int phone)
-    {
-        //縮放比例預設為1
-        float mScale = 1 ;
+    public void onPlateCheck(View v){
+        Intent it = new Intent(MainActivity.this, PlateCheckActivity.class);
+        startActivity(it);
+    }
 
-        //如果圖片寬度大於手機寬度則進行縮放，否則直接將圖片放入ImageView內
-        if(bitmap.getWidth() > phone )
-        {
-            //判斷縮放比例
-            mScale = (float)phone/(float)bitmap.getWidth();
+    public void onRoutePlan(View v){
+        Intent it = new Intent(MainActivity.this, RoutePlanActivity.class);
+        startActivity(it);
+    }
 
-            Matrix mMat = new Matrix() ;
-            mMat.setScale(mScale, mScale);
+    public void onComment(View v){
+        Intent it = new Intent(MainActivity.this, CommentActivity.class);
+        startActivity(it);
+    }
 
-            Bitmap mScaleBitmap = Bitmap.createBitmap(bitmap,
-                    0,
-                    0,
-                    bitmap.getWidth(),
-                    bitmap.getHeight(),
-                    mMat,
-                    false);
-            mImg.setImageBitmap(mScaleBitmap);
-        }
-        else mImg.setImageBitmap(bitmap);
+    public void onMember(View v){
+        Intent it = new Intent(MainActivity.this, MemberActivity.class);
+        Bundle bundle = new Bundle();
+        bundle.putString("userName", userName);
+        it.putExtras(bundle);
+        startActivity(it);
     }
 }
