85c218535ce3813388bd65c536a9c3b0d75b8f0a
d0280167 <xheroofxhero@gmail.com>
2017-06-13 20:29:17 +0800

    add some funtion

--- app/src/main/java/com/example/gjen/taxibar/PlateCheckActivity.java
+++ app/src/main/java/com/example/gjen/taxibar/PlateCheckActivity.java
@@ -366,4 +366,11 @@ public class PlateCheckActivity extends AppCompatActivity {
             }
         }
     }
+    //delete------------------------------
+    public static void main(String[] args) {
+        ReturnDemo1 v = new ReturnDemo1();
+        int result = v.add(11, 22);
+        System.out.println(result);
+    }
+    //delete---------------------------------
 }
